206de53503a64d3b476431a8780d178068250e27
Kashfia <kashfiabintae@gmail.com>
2024-09-21 17:03:04 +0600

    first commit

---
+++ Tupples Operations.py
@@ -0,0 +1,10 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = tuplex + ("Nahian",)
+print(tuplex)
+
+print(tuplex[4])
+
+tuple1 = (50, 10, 60, 70, 50, 50, 50)
+print(tuple1.count(50))
